9cd36c7936aa2a98420ab199cb4debf5a1a66603
rgan <rajaram.ganeshan@gmail.com>
2011-01-25 22:06:41 -0500

    Added google contacts retrieval - 2

--- teamsapp/models/team_member.rb
+++ teamsapp/models/team_member.rb
@@ -4,6 +4,7 @@ class TeamMember
   property :id, Serial
   property :name, String, :required => true, :length => 1..30
   property :facebook_id, String
+  property :google_id, String
 
   belongs_to :team, :required => false
 
--- teamsapp/test_environment.rb
+++ teamsapp/test_environment.rb
@@ -4,7 +4,7 @@ dir = File.join(File.dirname(file), ".gems/bundler_gems/jruby/1.8/gems")
 ["rspec", "diff-lcs", "extlib",
  "addressable", "dm-core", "dm-validations",
   "dm-appengine", "lexidecimal", "appengine-apis",
-  "sinatra", "tilt", "httparty", "crack", "mogli", "hashie"
+  "sinatra", "tilt", "httparty", "crack", "mogli", "hashie", "oauth", "jruby-openssl"
 
 ].each { |gem| Dir.glob(File.join(dir, gem + "*")).each {
     |item| $LOAD_PATH.unshift File.expand_path(File.join(item, "lib"))}
